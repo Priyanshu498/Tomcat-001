04a2c2572c5c2f6db741d305cc57fdf0b754afb8
Priyanshu498 <priyanshu1912003@gmail.com>
2024-09-18 02:53:29 +0530

    Update variables.tf

--- tomcat-infra/modules/alb/variables.tf
+++ tomcat-infra/modules/alb/variables.tf
@@ -1,1 +1,21 @@
+variable "security_group_id" {
+  type = string
+}
+
+variable "subnets" {
+  type = list(string)
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "port" {
+  type = number
+}
+
+variable "listener_port" {
+  type = number
+}
+
 
