f431d6fc6c131f12a7772b9026af935a9e56a57b
Priyanshu498 <priyanshu1912003@gmail.com>
2024-09-23 16:15:08 +0530

    Update main.tf

--- tomcat-infra/main.tf
+++ tomcat-infra/main.tf
@@ -96,6 +96,6 @@ module "alb" {
 # S3 Bucket Module
 module "s3" {
   source      = "./modules/s3"
-  bucket_name = "my-tomcat-test-bucket-45-ninja"
+  bucket_name = "my-tomcat-test-bucket-69-ninja"
 }
 
