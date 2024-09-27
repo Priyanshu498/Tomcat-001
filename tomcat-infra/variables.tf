1e468c312b6ea78847087bf8f29a04bf542bc65a
Priyanshu498 <priyanshu1912003@gmail.com>
2024-09-27 22:23:45 +0530

    Update variables.tf

--- tomcat-infra/variables.tf
+++ tomcat-infra/variables.tf
@@ -10,12 +10,12 @@ variable "aws_vpc" {
 # Subnets Configuration
 variable "public_subnet_cidrs" {
   type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   type    = list(string)
-  default = ["10.0.3.0/24", "10.0.4.0/24"]
+  default = ["10.0.5.0/24", "10.0.6.0/24"]
 }
 
 variable "availability_zones" {
